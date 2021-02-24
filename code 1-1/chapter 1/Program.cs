6ab59ce9dae3f551e498f7404f23e52f22260e46
yoohoo513 <yoohoo513@office365.kunsan.ac.kr>
2021-02-24 21:21:41 +0900

    챕터1 코드 1~3

---
+++ code 1-1/chapter 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace chapter_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("abc abc");
+            Console.Write("abc abc");
+            Console.Write("abc abc");
+        }
+    }
+}
