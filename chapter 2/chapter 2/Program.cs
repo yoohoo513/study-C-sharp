3f4c1989698d53fc6b17487737a3f36899601814
yoohoo513 <yoohoo513@office365.kunsan.ac.kr>
2021-02-25 19:35:28 +0900

    코드 2-6 정수형

---
+++ chapter 2/chapter 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace chapter_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1 + 2);
+            Console.WriteLine(1 * 2);
+            Console.WriteLine(1 / 2);
+            Console.WriteLine(1 - 2);
+            Console.WriteLine(1 % 2);
+
+        }
+    }
+}
